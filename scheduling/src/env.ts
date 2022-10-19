ab0b660f31ba4e22304fba8eb2cbea7a1a0ac0fd
femshima <49227365+femshima@users.noreply.github.com>
2022-10-19 15:53:21 +0900

    add docker-compose

--- scheduling/src/env.ts
+++ scheduling/src/env.ts
@@ -6,7 +6,9 @@ if (process.env['NODE_ENV'] !== 'production') (await import('dotenv')).config();
  * environment variables that are in use; always load from here
  */
 export const env = readenv({
-  mongoConnectionString: {},
+  mongoConnectionString: {
+    from: 'MONGO_CONNECTION_STRING',
+  },
   temporaryDirectory: {
     default: '/tmp',
   },
